c503fec58f6f74a035c54b2859db6ed89cb8ae7a
Cezar Miranda <cezar611@gmail.com>
2023-09-20 23:12:41 -0300

    Repetitions with logical test

---
+++ aula13/ambiente.js
@@ -0,0 +1,20 @@
+
+var c = 1
+while (c <= 15) {
+
+console.log(`Passo ${c}`)
+ c++ /* c = C + 1 */
+
+}
+
+
+
+
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/
+
