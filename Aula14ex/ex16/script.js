7607f0b6410b1ff9c47e19140327b64f82fe8264
Cezar Miranda <cezar611@gmail.com>
2023-09-27 23:57:46 -0300

    Creating a count system

--- Aula14ex/ex16/script.js
+++ Aula14ex/ex16/script.js
@@ -1,1 +1,35 @@
-alert('Olá!')
+function contar() {
+
+var inicio = document.getElementById('start')
+var final = document.getElementById('end')
+var passo = document.getElementById('step')
+var res = document.getElementById('res')
+
+   if (inicio.value.length == 0 || final.value.length == 0 || passo.value.length == 0) {
+    res.innerHTML = 'Impossível contar!'
+    window.alert('[ERRO] Faltam dados!')
+    
+   } else {
+    res.innerHTML = 'Contando: '
+    let i = Number(inicio.value)
+    let f = Number(final.value)
+    let p = Number(passo.value)
+
+
+    if (i < f) {
+    for(let c = i; c <= f; c += p) {
+
+        res.innerHTML += `${c} \u{1F449} `
+
+    }
+   
+   } else {
+
+    for (let c = i; c >= f; c -= p) {
+        res.innerHTML += ` ${c} \u{1F449}`
+
+    }
+    }
+    res.innerHTML += `\u{1f3c1}`
+  }
+}
