99ff223c0579bacd2a093ac6b6ec4083230b2988
Cezar Miranda <cezar611@gmail.com>
2023-09-28 22:48:28 -0300

    resolving the exercise alone with my mind

--- Aula14ex/ex16/script.js
+++ Aula14ex/ex16/script.js
@@ -7,14 +7,17 @@ var res = document.getElementById('res')
 
    if (inicio.value.length == 0 || final.value.length == 0 || passo.value.length == 0) {
     res.innerHTML = 'Impossível contar!'
-    window.alert('[ERRO] Faltam dados!')
+    //window.alert('[ERRO] Faltam dados!')
     
    } else {
-    res.innerHTML = 'Contando: '
+    res.innerHTML = 'Contando: <br>'
     let i = Number(inicio.value)
     let f = Number(final.value)
     let p = Number(passo.value)
-
+    if (p <= 0) {
+            window.alert('Passo inválido! Considerando Passo 1')
+       p = 1
+    }
 
     if (i < f) {
     for(let c = i; c <= f; c += p) {
