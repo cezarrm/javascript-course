27d178e01542c2a9efa7ccd2f4c470f2eee3cd7f
Cezar Miranda <cezar611@gmail.com>
2023-12-04 23:39:54 -0300

    Create ambiente.js

---
+++ aula15/ambiente.js
@@ -0,0 +1,25 @@
+let valores = [ 0, 1, 9, 8, 4, 2, 9]
+//for(let pos = 0; pos <valores.length; pos++){ // percurso para exibição
+
+   // console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+
+
+//}
+
+/*
+for(let pos in valores){
+
+console.log(valores[pos])
+
+}
+*/
+/*
+for(let pos in valores){
+
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+
+
+}
+*/
+let pos = valores.indexOf(3)
+console.log(`O valor 8 está na posição ${pos}`)
