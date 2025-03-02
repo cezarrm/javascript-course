aadcfcc34cc32ff29eab52b6b532b41d304bf025
cezarrm <cezar611@gmail.com>
2025-03-01 23:58:07 -0300

    nova aula

---
+++ aula_58/script.js
@@ -0,0 +1,26 @@
+//funçãoc construtora -> objetos cria objetos e retorna objeto (precisa mudar convensão)
+// precisa começar com letra maiuscula (Pessoa) precisa usar a palavra new
+// função fabrica -> objetos (criaPessoa)
+function Pessoa(nome, sobrenome) { //molde
+    const ID = 123456; //variavel privada só vive dentro desse escopo
+
+    const metodoInterno = () => {
+
+
+    };
+
+    this.nome = nome
+    this.sobrenome = sobrenome
+    //n precisa retornar nada
+
+    this.metodo = () => {
+        console.log(this.nome + ' sou um método!')
+
+    };
+}
+
+const p1 = new Pessoa('Luiz','Otavio'); //cria objeto com os moldes PESSOA precisa usar a palavra new
+const p2 = new Pessoa('Maria','Oliveira');
+
+p1.metodo();
+p2.metodo();
