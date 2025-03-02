6aa79079e7dacca403140d06232dff7bff4a3f58
cezarrm <cezar611@gmail.com>
2025-03-02 15:55:55 -0300

    using constructors functions

---
+++ aula_58/modelo/assets/js/main.js
@@ -0,0 +1,87 @@
+function Calculator () { 
+
+    this.display = document.querySelector('.display');
+    
+    
+    
+    
+    
+    this.Start =() => {
+
+        this.ClickButton()
+        
+    }
+    
+    
+    this.ClickButton = () => { 
+
+        document.addEventListener('click', (e) => {
+            const el = e.target;
+            
+           if(el.classList.contains('btn-num')) {
+                this.addDisplay(el.innerText);
+           }
+           
+           if(el.classList.contains('btn-clear')) {
+            this.clearDisplay();
+           }
+
+           if(el.classList.contains('btn-del')) {
+            this.deleteLast();
+           }
+
+           if(el.classList.contains('btn-eq')) {
+            this.makeCount();
+           }
+         
+        });
+    };
+
+
+    this.makeCount = () => {
+        let count = this.display.value;
+
+        try { 
+
+            count = eval(count);
+            
+            if(!count) {
+                alert('conta inválida!')
+                return; 
+
+            }
+
+            this.display.value = String(count)
+        } catch(e){
+
+            alert('Conta Inválida!')
+            return;
+        }
+
+    }
+
+    this.addDisplay = (value) => {
+
+        this.display.value += value;
+
+    }
+
+    this.clearDisplay = () => {
+
+        this.display.value = '';
+        
+    }
+
+    this.deleteLast = () => {
+
+        this.display.value = this.display.value.slice(0, -1);
+
+    }
+    
+};
+
+
+
+const calculator = new Calculator();
+
+calculator.Start();
--- aula_58/script.js
+++ aula_58/script.js
@@ -2,13 +2,14 @@
 // precisa começar com letra maiuscula (Pessoa) precisa usar a palavra new
 // função fabrica -> objetos (criaPessoa)
 function Pessoa(nome, sobrenome) { //molde
+    //atributos ou métodos privados
     const ID = 123456; //variavel privada só vive dentro desse escopo
 
     const metodoInterno = () => {
 
 
     };
-
+    //atributos ou métodos públicos (pode acessar fora da função)
     this.nome = nome
     this.sobrenome = sobrenome
     //n precisa retornar nada
