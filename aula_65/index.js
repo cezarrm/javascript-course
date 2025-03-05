3748aac50f3c3cc6fc6721e0612dd2fce25f2d9e
cezarrm <cezar611@gmail.com>
2025-03-05 18:56:50 -0300

    learning filter

---
+++ aula_65/index.js
@@ -0,0 +1,18 @@
+//filter, map, reduce
+
+//retorne os números maiores que 10
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+const maior = [];
+
+for (let i of numeros) {
+     if (i > 10) {
+
+       maior.push(i) 
+
+     }
+console.log(i)
+
+}
+
+console.log(maior)
