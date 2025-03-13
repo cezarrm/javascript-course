dcf26a3f5c78fafb546adfe85e0038c0d4fb7232
cezarrm <cezar611@gmail.com>
2025-03-13 00:05:12 -0300

    just fixing some stuffs

--- aula_73/index.js
+++ aula_73/index.js
@@ -1,7 +1,18 @@
 //objetos são valores passados por referência;
 //como copiar objetos
 
-const produto = {nome: 'Produto', preco: '1.8'};
+const produto = {nome: 'Produto', preco: '1.8', material: 'porcelana'};
+//console.log(Object.values(produto)) //pega os valores, não pega as chaves;
+//console.log(Object.entries(produto)) //pega os valores e keys, retorna um array maior, com arrays internos
+
+for (let valor of Object.entries(produto)) {
+
+    console.log(valor[0], valor [1]); //cada iteração pega um array com dois objetos; da pra fazer destructor
+
+}
+
+/*
+
 Object.defineProperty(produto, 'nome', {
     writable: false,
     configurable: false,    
@@ -14,3 +25,20 @@ console.log(Object.getOwnPropertyDescriptor(produto, 'nome'))
 console.log(produto)
 
 
+*/
+
+/* 
+object.values (retorna valores)
+object. entries (retorna chaves e valores em arrays)
+object.getOwnPropertyDescripton(o, 'prop); retorna o descritor daquela propriedade dentro do objeto
+object.assign(des, any) serve pra copiar um objeto, e vários outros objetos
+...(spread)
+
+//javimos
+
+object.keys - retorna chaves
+object.freeza - trava objeto completo
+object.defineProperties - define propriedades com seus descritores
+object.defineProperty - define uma propriedade;
+
+*/
