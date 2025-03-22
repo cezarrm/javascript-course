a27f442206c036bad7edbc6bfdfbfa11df56696f
cezarrm <cezar611@gmail.com>
2025-03-21 21:05:29 -0300

    exercises about polymorphism

--- aula_79/REVIEW/index2.js
+++ aula_79/REVIEW/index2.js
@@ -44,7 +44,7 @@ Pessoa.prototype.saudar = function (){
 const pessoa1 = new Pessoa('Cezar','Miranda');
 console.log(pessoa1.saudar())
 
-*/ 
+
 //
 //Exercício: Crie um objeto animal e use Object.create() para gerar um novo objeto gato que herda de animal.
 
@@ -68,7 +68,7 @@ gato1.nome = 'Felino';
 gato1.idade = 23;
 
 gato1.printTela();
-
+*/ 
 /* 
 Herança
 
@@ -77,7 +77,9 @@ A herança pode ser feita usando prototype ou class.
 Exercício:
 
 Crie uma classe Funcionario e uma classe Gerente que herda de Funcionario.
+*/ 
 
+/*
 Polimorfismo
 
 Polimorfismo permite que métodos compartilhem o mesmo nome, mas tenham comportamentos diferentes.
@@ -85,7 +87,46 @@ Polimorfismo permite que métodos compartilhem o mesmo nome, mas tenham comporta
 Exercício:
 
 Crie uma classe Instrumento com um método tocar() e uma classe Violao que sobrescreve esse método.
+*/
 
+function Instrumento (nome) {
+
+    this.nome = nome;
+
+};
+
+Instrumento.prototype.tocar = function (nome) {
+
+    return `O ${this.nome} está tocando`
+
+};
+
+function Violao (nome, musica, artista) {
+
+    Instrumento.call(this, nome); //puxa o que eu quero herdar do object
+  
+    this.musica = musica;
+    this.artista = artista;
+
+}
+
+Violao.prototype = Object.create(Instrumento.prototype) //para herdar corretamente os métodos de instrumento;
+Violao.prototype.constructor = Violao // define o proto de Violao
+
+Violao.prototype.tocar = function () {
+
+    return `O ${this.nome} está tocando "${this.musica}" - ${this.artista}`;
+
+
+}
+
+const violao = new Violao ('Violao','Essa tal liberdade','Só pra contrariar')
+
+
+
+console.log(violao.tocar())
+
+/*
 Factory Functions + Prototypes
 
 Factory Functions criam objetos sem a necessidade de new.
@@ -93,7 +134,9 @@ Factory Functions criam objetos sem a necessidade de new.
 Exercício:
 
 Crie uma Factory Function criarProduto que retorna um objeto com nome e preço.
+*/ 
 
+/*
 Objeto Map()
 
 O Map permite armazenar pares chave-valor com qualquer tipo de dado.
